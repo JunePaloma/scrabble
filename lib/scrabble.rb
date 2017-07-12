a0cc11128282832faebc78fa7a62cb550520e70d
JunePaloma <kborkoski@gmail.com>
2017-07-12 09:47:34 -0600

    Add pseudocode for scoring words

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,27 +1,46 @@
 class Scrabble
-attr_reader @point_values,
-            @score,
-            @word
+attr_reader :score,
+            :word
 
-  def initialize(word)
+  def initialize
     @word = word
+    @point_values = {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
 end
 
 def break_word_into_an_array(word)
-  word_array = word.chars
+  unless word == "" || word == nil
+  word_array = word.upcase.chars #now I have the array ["H", "E", "L", "L", "O"]
 end
 
-def add_up_string_values(word_array)
-
-  #this should give
+def get_individual_letters(word_array)
+  until word_array == []
+  score(word_array.pop) #until the array is empty, pop off letters and feed them into the letter-scoring method
+  end
 end
 
-  def score(word_array)
-  # increment count count = sum of the values from the points hash for the word
+  def score(letter)
+    score = []
+    point_values.each do |letter|
+      if letter == point_values.key
+        score << point_values.value #compare each letter from the word array to the keys in point_values, and shovel those values into an a new array
+      end
+      return score #this should be the array [1,2,1,1,1], for "hello"
   end
 
+def word_score()
+  #(1) inject the score array to return the values added up, && (2) multiply by the multiplier if it exists. 
+end
+
   def point_values
-    {
+    point_values = {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -31,9 +50,6 @@ end
       "Y"=>4, "Z"=>10
     }
   end
-
-
-
 
 
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -6,8 +6,19 @@ require 'pry'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_it_creates_an_array
+    newword = Scrabble.new
+    expected = ["h", "e", "l", "l", "o"]
+    actual = Scrabble.new.break_word_into_an_array("hello")
+    assert_equal expected, actual
+  end
+
+  def test_it_can_score_a_word
+  end
+
+  
 end
